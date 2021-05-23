b6f56ad2247b7ec2ed39a0c476411c262daf8cd7
kyle <kylercm@gmail.com>
2021-05-23 16:32:17 +1200

    fix nat type

--- start.sh
+++ start.sh
@@ -6,7 +6,6 @@ if [ ! -f ./ubuntu-16.04.7-server-amd64.iso ]; then
 fi
 
 VBoxManage createvm --name $VM --ostype "Linux_64" --register
-VBoxManage createhd --filename /VirtualBox/$VM/$VM.vdi --size 32768
 VBoxManage storagectl $VM --name "SATA Controller" --add sata --controller IntelAHCI
 VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 --device 0 \
 --type hdd --medium /VirtualBox/$VM/$VM.vdi
@@ -14,7 +13,7 @@ VBoxManage storagectl $VM --name "IDE Controller" --add ide
 VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0 --device 0 \
 --type dvddrive --medium ./ubuntu-16.04.7-server-amd64.iso
 
-VBoxManage modifyvm $VM --memory 8192
+VBoxManage modifyvm $VM --memory 8192 --nic1 nat --natpf1 rule1,tcp,127.0.0.1,2222,10.0.2.15,22
 
 VBoxManage unattended install $VM \
 --iso= ./ubuntu-16.04.7-server-amd64.iso\
