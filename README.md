e8a49fbefce95ed73eb97c40e26b1fa2700962e5
kyle <kylercm@gmail.com>
2021-06-10 17:22:19 +1200

    final just making sure its all up there

--- README.md
+++ README.md
@@ -9,16 +9,16 @@ only to allow password-less (certificate) based login
 then display the MOTD including the text "Hello Assurity DevOps”.
 
 This was done in a simple bash script using virtualbox as it was the simplest to set up in a short time
-However it could easily be adapted to run in a playbook
+However it could easily be adapted to run in a playbook (some of it at least)
 I will put in considerations I had into brackets for things I would have liked to add if I had more time and other details.
 (for example using a playbook for this whole exercise would have been optimal but I could not guarantee tool dependancies and I don't currently have an Ansible implementation at home)
 
 # assumptions
 
-You have virtualbox installed on an ubuntu server >19
+You have virtualbox 6.1.16 installed on an Ubuntu Server
+(tested working in Ubuntu Desktop 20.04 and Server 18.04)
 >1024MB of available memory
 >10GB of available space in /
-
 
 # usage
 
@@ -32,9 +32,14 @@ sudo ./start.sh
 
 The script will download the ubuntu-16.04.7-server-amd64 iso into the current location of the script
 If you already have the iso simply put it in the same directory to avoid re-download 
-(Would have liked more controlled data entry like specifing iso and install location)
+(You could change the argument to use a different ISO as well)
 
 While start.sh kicks off the virtualbox vm a bulk of the work is done in the ubuntu preseed.
 Default root password is set there but not used
 Had a few issues with the default preseed causing some Kernal Panics so had to get a fix (Thanks Rob Raymond of StackExchange)
 If i was using an anisble playbook for this I would have a lot of the variables I've set in there available to change especially the Kernal Version but also login username, root password, MOTD, ip addresses etc
+
+Once the ISO is downloaded/found the script will set up the VM config with virtualbox
+then it will begin the unattended installation (How long this takes had me worried but I'm used to containers and prebuilt images)
+after building starts the script will wait for it to show up on ssh (the preseed configures ssh)
+It will use virtualbox to hand over the keys then finally connect with the key so you would be connected to it in your terminal.
--- start.sh
+++ start.sh
@@ -2,20 +2,28 @@
 VM="ubuntu-16"
 OS="Ubuntu_64" 
 iso="ubuntu-16.04.7-server-amd64.iso"
+release="16.04.7"
 hddsize=32768
 LocalHOSTIP=127.0.0.1
 AccessPORT=2222
 username="login"
 password="changeme"
-TIMEOUT=30
+TIMEOUT=60
+LOG_FILE="log.log"
+RSAname="rsa"
 
-ssh-keygen -t rsa -f rsa -P $password
+exec 3>&1 1>>${LOG_FILE} 2>&1
+
+echo "Generating a key" 1>&3
+ssh-keygen -t rsa -f $RSAname -P $password
 
 #check we got iso
 if [ ! -f ./$iso ]; then
-    curl https://releases.ubuntu.com/16.04.7/$iso -o ./$iso
+	echo "Downloaing the ISO..." 1>&3
+    curl https://releases.ubuntu.com/$release/$iso -o ./$iso
 fi
 
+echo "setting the VM details" 1>&3
 #set vm details
 VBoxManage createvm --name $VM --ostype $OS --register
 VBoxManage createhd --filename /VirtualBox/$VM/$VM.vdi --size $hddsize
@@ -31,6 +39,7 @@ VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0 --device 0 \
 #set memory and nic config
 VBoxManage modifyvm $VM --memory 8192 --nic1 nat --natpf1 rule1,tcp,$LocalHOST,$AccessPORT,10.0.2.15,22
 
+echo "Starting the build" 1>&3
 #set unattended install details
 VBoxManage unattended install $VM \
 --iso=./$iso \
@@ -38,40 +47,44 @@ VBoxManage unattended install $VM \
 --install-additions --script-template=./ubuntu_preseed.cfg \
 --start-vm=headless 
 
-echo "We're building the VM. Please wait..."
+echo "We're building the VM. Please wait..." 1>&3
 #this can take a while.
-sleep 2m
+sleep 3m
 
 while :; do 
-    echo "Waiting for VM to be up..."
+    echo "Waiting for VM to be up..." 1>&3
     # https://serverfault.com/questions/152795/linux-command-to-wait-for-a-ssh-server-to-be-up
     # https://unix.stackexchange.com/questions/6809/how-can-i-check-that-a-remote-computer-is-online-for-ssh-script-access
     # https://stackoverflow.com/questions/1405324/how-to-create-a-bash-script-to-check-the-ssh-connection
-    status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 ${LocalHOST} -p ${AccessPORT} echo ok 2>&1)
+    status=$(ssh -o BatchMode=yes -o ConnectTimeout=10 ${LocalHOST} -p ${AccessPORT} echo ok 2>&1)
     RESULT=$?
     if [ $RESULT -eq 0 ]; then
         # this is not really expected unless a key lets you log in
-        echo "connected ok"
+        echo "connected ok" 1>&3
         break
     fi
     if [ $RESULT -eq 255 ]; then
         # connection refused also gets you here
         if [[ $status == *"Permission denied"* ]] ; then
             # permission denied indicates the ssh link is okay
-            echo "server response found"
+            echo "server response found" 1>&3
             break
         fi
     fi
     TIMEOUT=$((TIMEOUT-1))
     if [ $TIMEOUT -eq 0 ]; then
-        echo "timed out"
-        # error for jenkins to see
+        echo "timed out trying again" 1>&3
+        # error for automation controller to see
         exit 1 
     fi
     sleep 10
 done
 
+echo "Putting the key onto the host" 1>&3
+#put the key onto the VM
 VBoxManage guestcontrol $VM --username $username --password $password \
-copyto rsa.pub --target-directory /home/$username/.ssh/authorized_keys
+copyto rsa.pub --target-directory /home/$username/.ssh/authorized_keys \
+|| echo "Failed to put the key onto the host, did the addtions install correctly?" 1>&3
 
-ssh -i rsa -l $username $LocalHOST -p $AccessPORT
+echo "Finally attempt to connect with the key over ssh" 1>&3
+ssh -i rsa -l $username $LocalHOST -p $AccessPORT 
